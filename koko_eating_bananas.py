32a3333c698f19c2a64b812e916a12757a978650
suleymangunes <suleymangunesmail@gmail.com>
2024-12-06 17:21:20 +0300

    LeetCode 75 Question 5 Solved

---
+++ koko_eating_bananas.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def canFinish(k):
+            total_hours = 0
+            for pile in piles:
+                total_hours += (pile + k - 1) // k
+            return total_hours <= h
+        
+        left, right = 1, max(piles)
+        result = right
+        while left <= right:
+            mid = (left + right) // 2
+            if canFinish(mid):
+                result = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return result
