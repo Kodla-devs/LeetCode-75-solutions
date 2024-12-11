d6fb73be1de0e6696ad68dcf98e6d48d1cb07022
suleymangunes <suleymangunesmail@gmail.com>
2024-12-11 21:08:31 +0300

    LeetCode 75 Question 64 Solved

---
+++ longest_common_subsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        if len(text1) < len(text2):
+            text1, text2 = text2, text1
+        
+        prev = [0] * (len(text2) + 1)
+        curr = [0] * (len(text2) + 1)
+
+        for i in range(1, len(text1) + 1):
+            for j in range(1, len(text2) + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    curr[j] = prev[j - 1] + 1
+                else:
+                    curr[j] = max(prev[j], curr[j - 1])
+            prev, curr = curr, [0] * (len(text2) + 1)
+        
+        return prev[len(text2)]
